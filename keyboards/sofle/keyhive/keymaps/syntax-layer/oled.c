3a1870762f1d75e477e8e5cff401eeb12baf89ef
Josh Bax <joshbax189@gmail.com>
2025-07-04 11:09:42 -0700

    Fix sofle oled.c to use _user functions overriding sofle.c

--- keyboards/sofle/keyhive/keymaps/syntax-layer/oled.c
+++ keyboards/sofle/keyhive/keymaps/syntax-layer/oled.c
@@ -1,23 +1,6 @@
- /* Copyright 2020 Josef Adamcik
-  * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+// see functions in sofle.c in main directory
 
-//Sets up what the OLED screens display.
-
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {
@@ -29,7 +12,7 @@ static void render_logo(void) {
     oled_write_P(qmk_logo, false);
 }
 
-static void print_status_narrow(void) {
+static void print_my_status(void) {
     // Print current layer
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
@@ -48,24 +31,17 @@ static void print_status_narrow(void) {
     oled_write_P(PSTR("\n\n"), false);
 
     led_t led_usb_state = host_keyboard_led_state();
-    if (led_usb_state.caps_lock) {
-        oled_write_ln_P(PSTR("CPSLK"), true);
-    }
+    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 }
 
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+bool oled_task_user(void) {
     if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
-}
-
-void oled_task_user(void) {
-    if (is_keyboard_master()) {
-        print_status_narrow();
+        print_my_status();
     } else {
         render_logo();
     }
+    // don't run oled_task_kb
+    return false;
 }
 
 #endif
