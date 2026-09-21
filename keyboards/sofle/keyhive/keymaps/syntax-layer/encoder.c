5328ec7a1ebc6061636c16948684204883a5dd37
Josh Bax <joshbax189@gmail.com>
2026-09-21 16:42:58 -0700

    Fix encoder functions

--- keyboards/sofle/keyhive/keymaps/syntax-layer/encoder.c
+++ keyboards/sofle/keyhive/keymaps/syntax-layer/encoder.c
@@ -20,9 +20,9 @@
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
+    if (index == 0) { // left encoder
         // rhs mod pressed
-        if (layer_state_is(2)) {
+        if (layer_state_is(1)) {
             if (clockwise) {
                 tap_code(KC_UP);
             } else {
@@ -35,14 +35,23 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 tap_code(KC_LEFT);
             }
         }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
+    } else if (index == 1) { // right encoder
+        if (layer_state_is(2)) {
+            if (clockwise) {
+                tap_code(KC_PGDN);
+            } else {
+                tap_code(KC_PGUP);
+            }
         } else {
-            tap_code(KC_PGUP);
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
         }
     }
-    return true;
+    // override default behavior
+    return false;
 }
 
 #endif
